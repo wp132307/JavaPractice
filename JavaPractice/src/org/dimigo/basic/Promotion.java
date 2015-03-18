59eae68791264f35ea39a7467b300a03f9d4c3c4
wp132307 <tnwjd6249@naver.com>
2015-03-18 14:46:23 +0900

    2차 과제 제출

--- JavaPractice/src/org/dimigo/basic/Promotion.java
+++ JavaPractice/src/org/dimigo/basic/Promotion.java
@@ -8,7 +8,7 @@ public class Promotion {
 		int store = 1500; 
 		System.out.println("<<디미베네 연간 인건비>>");
 		System.out.println("월 평균 급여 : " + String.format("%,d원",mMoney));
-		System.out.println("점포 내 직원수 : " + String.format("%,d명",clerk));
+		System.out.println("점포 내 직원수 : " + clerk + "명");
 		System.out.println("점포 수 : " + String.format("%,d개",store));
 		System.out.println("연간 인건비 : " + String.format("%,d원", (long)mMoney * 12 * clerk * store));
 
