b3c765a4b7736cfa748c325932bbad6f90cc91f4
wp132307 <tnwjd6249@naver.com>
2015-04-14 11:36:17 +0900

    제 7차 과제 제출

---
+++ JavaPractice/src/org/dimigo/oop/CarTest.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ * 	 |_ CarTest
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2015. 4. 14.
+ * </pre>
+ * 
+ * @author		:SAMSUNG
+ * @version		:1.0
+ */
+public class CarTest {
+
+	
+	public static void main(String[] args) {
+		Car car = new Car();
+		
+		System.out.println("<< 자동차 목록 >>");
+		System.out.println("제조사명 : " + car.getCompany());
+		System.out.println("모델명 : " + car.getModel());
+		System.out.println("색상 : " + car.getColor());
+		System.out.println("최대속도 : " + car.getMaxSpeed() + "km");
+		System.out.println("가격 : " + String.format("%,d원", car.getPrice()));
+		System.out.println();
+		
+		car.setCompany("기아자동차");
+		car.setModel("K7");
+		car.setColor("흰색");
+		car.setMaxSpeed(246);
+		car.setprice(40000000);
+		
+		System.out.println("<< 자동차 목록 >>");
+		System.out.println("제조사명 : " + car.getCompany());
+		System.out.println("모델명 : " + car.getModel());
+		System.out.println("색상 : " + car.getColor());
+		System.out.println("최대속도 : " + car.getMaxSpeed() + "km");
+		System.out.println("가격 : " + String.format("%,d원", car.getPrice()));
+		System.out.println();
+		
+		car.setCompany("삼성자동차");
+		car.setModel("SM7");
+		car.setColor("회색");
+		car.setMaxSpeed(200);
+		car.setprice(38000000);
+		
+		System.out.println("<< 자동차 목록 >>");
+		System.out.println("제조사명 : " + car.getCompany());
+		System.out.println("모델명 : " + car.getModel());
+		System.out.println("색상 : " + car.getColor());
+		System.out.println("최대속도 : " + car.getMaxSpeed() + "km");
+		System.out.println("가격 : " + String.format("%,d원", car.getPrice()));
+		System.out.println();
+		
+	}
+
+}
