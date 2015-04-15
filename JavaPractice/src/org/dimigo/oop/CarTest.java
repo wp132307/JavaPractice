add4df01644d63db111edd4607c1841904f0ad88
wp132307 <tnwjd6249@naver.com>
2015-04-15 14:46:30 +0900

    제 7-2 과제 제출

--- JavaPractice/src/org/dimigo/oop/CarTest.java
+++ JavaPractice/src/org/dimigo/oop/CarTest.java
@@ -19,7 +19,10 @@ public class CarTest {
 
 	
 	public static void main(String[] args) {
-		Car car = new Car();
+		//Car car = new Car();
+		Car2 car = new Car2();
+		Car2 car2 = new Car2("기아자동차","K7","검정색",246,40000000);
+		Car2 car3 = new Car2("삼성자동차","SM7","회색",200,38000000);
 		
 		System.out.println("<<자동차 목록>>");
 		System.out.println("제조사명 : " + car.getCompany());
@@ -29,30 +32,30 @@ public class CarTest {
 		System.out.println("가격 : " + String.format("%,d원", car.getPrice()));
 		System.out.println();
 		
-		car.setCompany("기아자동차");
-		car.setModel("K7");
-		car.setColor("흰색");
-		car.setMaxSpeed(246);
-		car.setprice(40000000);
+//		car.setCompany("기아자동차");
+//		car.setModel("K7");
+//		car.setColor("흰색");
+//		car.setMaxSpeed(246);
+//		car.setprice(40000000);
 		
-		System.out.println("제조사명 : " + car.getCompany());
-		System.out.println("모델명 : " + car.getModel());
-		System.out.println("색상 : " + car.getColor());
-		System.out.println("최대속도 : " + car.getMaxSpeed() + "km");
-		System.out.println("가격 : " + String.format("%,d원", car.getPrice()));
+		System.out.println("제조사명 : " + car2.getCompany());
+		System.out.println("모델명 : " + car2.getModel());
+		System.out.println("색상 : " + car2.getColor());
+		System.out.println("최대속도 : " + car2.getMaxSpeed() + "km");
+		System.out.println("가격 : " + String.format("%,d원", car2.getPrice()));
 		System.out.println();
 		
-		car.setCompany("삼성자동차");
-		car.setModel("SM7");
-		car.setColor("회색");
-		car.setMaxSpeed(200);
-		car.setprice(38000000);
+//		car.setCompany("삼성자동차");
+//		car.setModel("SM7");
+//		car.setColor("회색");
+//		car.setMaxSpeed(200);
+//		car.setprice(38000000);
 		
-		System.out.println("제조사명 : " + car.getCompany());
-		System.out.println("모델명 : " + car.getModel());
-		System.out.println("색상 : " + car.getColor());
-		System.out.println("최대속도 : " + car.getMaxSpeed() + "km");
-		System.out.println("가격 : " + String.format("%,d원", car.getPrice()));
+		System.out.println("제조사명 : " + car3.getCompany());
+		System.out.println("모델명 : " + car3.getModel());
+		System.out.println("색상 : " + car3.getColor());
+		System.out.println("최대속도 : " + car3.getMaxSpeed() + "km");
+		System.out.println("가격 : " + String.format("%,d원", car3.getPrice()));
 		System.out.println();
 		
 	}
